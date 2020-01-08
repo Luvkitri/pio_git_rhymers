2ba937828c939fe279b20649e5f4c60a5a2d10e2
kombajno678 <38471959+kombajno678@users.noreply.github.com>
2020-01-08 11:41:01 +0100

    Revert "stale itd zrobione"
    This reverts commit 672e39d890d01f91878c283002f73d3776aeaf43.

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,11 +2,9 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int SIZE = 12;
-    public static final int ERROR = -1;
-    private int[] NUMBERS = new int[SIZE];
+    private int[] NUMBERS = new int[12];
 
-    public int total = ERROR;
+    public int total = -1;
 
     public void countIn(int in) {
         if (!isFull())
