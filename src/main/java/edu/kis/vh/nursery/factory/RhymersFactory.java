91c10e88cd73faa0aceda9401644f8d29dd8d14c
Luvkitri <46831775+Luvkitri@users.noreply.github.com>
2020-01-08 11:21:20 +0100

    1.3 commit

---
+++ src/main/java/edu/kis/vh/nursery/factory/RhymersFactory.java
@@ -0,0 +1,16 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+
+public interface RhymersFactory {
+
+    public DefaultCountingOutRhymer getStandardRhymer();
+
+    public DefaultCountingOutRhymer getFalseRhymer();
+
+    public DefaultCountingOutRhymer getFIFORhymer();
+
+    public DefaultCountingOutRhymer getHanoiRhymer();
+
+//    alt + -> i alt + <- sluza do przelaczania sie pomiedzy otwartymi tabami w danym projekcie
+}
